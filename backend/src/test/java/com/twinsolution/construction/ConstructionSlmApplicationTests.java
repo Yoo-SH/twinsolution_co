25cdfaaacce111e9473b13a9d834de9ecb185c6b
Yoo-SH <ads137485@jbnu.ac.kr>
2025-11-24 23:45:30 +0900

    build: spring boot 빌드 설정 - web, jpa, validation, actuator, lombok - db: h2

---
+++ backend/src/test/java/com/twinsolution/construction/ConstructionSlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twinsolution.construction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionSlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
